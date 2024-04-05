4a317c8c1431740e2a7ed20dcc429c8d37d92fba
pavel-stastny <pavel.stastny@gmail.com>
2024-04-05 18:41:31 +0200

    Make dir

--- create-zip.js
+++ create-zip.js
@@ -2,6 +2,8 @@ console.log("!!!! ARCHIVING ....");
 const fs = require('fs');
 const archiver = require('archiver');
 
+if (!fs.existsSync(__dirname + '/dist-zip')) { fs.mkdirSync(__dirname + '/dist-zip'); }
+
 const output = fs.createWriteStream(__dirname + '/dist-zip/kramerius-admin.zip');
 const archive = archiver('zip', {
     zlib: { level: 9 } 
