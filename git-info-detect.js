175dc773e3346c6d29023ba47adb2f253b11262b
pavel-stastny <pavel.stastny@gmail.com>
2024-04-05 17:34:46 +0200

    Debug message

--- git-info-detect.js
+++ git-info-detect.js
@@ -4,4 +4,6 @@ const { writeFileSync } = require('fs');
 const info = gitDescribeSync();
 const versionInfoJson = JSON.stringify(info, null, 2);
 
+console.log(">>>>>>>>>>>>>>>>>> JE to tady !!!!!");
+
 writeFileSync('git-info.json', versionInfoJson);
